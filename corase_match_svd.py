3d98183d4e60c9b6c8a81b45a5477baf312a6cdb
Ruichen Zheng <ankbzpx@hotmail.com>
2023-03-30 13:36:06 +0800

    corase is sufficient

--- corase_match_svd.py
+++ corase_match_svd.py
@@ -1,26 +1,21 @@
 import numpy as np
-from icecream import ic
 import trimesh
 import polyscope as ps
 
-# Depends on GPU memory
-kMaxQuerySize = 2e9
 
-
-def match_correspondence(P, Q, matched):
+def match_correspondence(P, Q, matches):
     P_copy = np.copy(P)
-    Q_copy = np.copy(Q)
 
-    P = P[matched[:, 0]]
-    Q = Q[matched[:, 0]]
+    P = P[matches[:, 0]]
+    Q = Q[matches[:, 0]]
 
     P_mu = np.average(P, 0, keepdims=True)
     P_bar = P - P_mu
-    Q = scan.vertices[known_correspondences[:, 1]]
+    Q = scan.vertices[matches[:, 1]]
     Q_mu = np.average(Q, 0, keepdims=True)
     Q_bar = Q - Q_mu
 
-    cov = Q_bar.T @ P_bar / len(known_correspondences)
+    cov = Q_bar.T @ P_bar / len(matches)
     U, S, V_T = np.linalg.svd(cov)
     R = U @ V_T
 
@@ -29,12 +24,12 @@ def match_correspondence(P, Q, matched):
     E[-1] = np.sign(np.linalg.det(R))
     R = R * E
 
-    demo = np.sum(P_bar * P_bar) / len(known_correspondences)
+    demo = np.sum(P_bar * P_bar) / len(matches)
 
     s = np.sum(S) / demo
     t = Q_mu - s * P_mu @ R.T
 
-    return s * P_copy @ R.T + t, Q_copy
+    return s * P_copy @ R.T + t
 
 
 if __name__ == '__main__':
@@ -47,23 +42,32 @@ if __name__ == '__main__':
                                          process=False,
                                          maintain_order=True)
 
-    known_correspondences = np.array([[10274, 974177], [10304, 1254944],
-                                      [12331, 965655], [18558, 742479],
-                                      [123, 968848], [256, 920117],
-                                      [4778, 1076107], [15193, 644677],
-                                      [13022, 908601], [20009, 235969],
-                                      [9692, 362182], [133, 867122]])
+    # hand pick vertices (double click)
+    # ps.init()
+    # ps.register_point_cloud("template", template.vertices)
+    # ps.show()
 
-    VX, VY = match_correspondence(template.vertices,
-                                  scan.vertices,
-                                  matched=known_correspondences)
+    # ps.init()
+    # ps.register_point_cloud("scan", scan.vertices)
+    # ps.show()
+
+    # exit()
+
+    # TODO: Auto detect or finetone the matches
+    matches = np.array([[10274, 974177], [10304, 1254944], [12331, 965655],
+                        [18558, 742479], [123, 968848], [256, 920117],
+                        [4778, 1076107], [15193, 644677], [13022, 908601],
+                        [20009, 235969], [9692, 362182], [133, 867122],
+                        [18463, 303717], [8155, 298427], [15065, 616624],
+                        [4749, 1095784]])
+
+    VX = match_correspondence(template.vertices, scan.vertices, matches=matches)
 
     trimesh.Trimesh(VX, template.faces).export('template_corase_match.obj')
-    trimesh.Trimesh(VY, scan.faces).export('scan_corase_match.obj')
 
     ps.init()
     ps.register_surface_mesh("template", VX, template.faces)
-    ps.register_surface_mesh("scan", VY, scan.faces)
-    ps.register_point_cloud("template kpt", VX[known_correspondences[:, 0]])
-    ps.register_point_cloud("scan kpt", VY[known_correspondences[:, 1]])
+    ps.register_surface_mesh("scan", scan.vertices, scan.faces)
+    ps.register_point_cloud("template kpt", VX[matches[:, 0]])
+    ps.register_point_cloud("scan kpt", scan.vertices[matches[:, 1]])
     ps.show()
